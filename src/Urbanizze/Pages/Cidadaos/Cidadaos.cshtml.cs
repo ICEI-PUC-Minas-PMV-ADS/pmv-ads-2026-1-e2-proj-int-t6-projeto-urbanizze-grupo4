5940b827b441da023d64b04b155f3810dda7b946
Viniciusmfs0421 <vinicius.sousa@sga.pucminas.br>
2026-05-25 22:59:14 -0300

    criado tela cidadoes, tanto no mock, quanto rodando com os controllers

---
+++ src/Urbanizze/Pages/Cidadaos/Cidadaos.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+ 
+namespace SeuProjeto.Pages.Cidadaos
+{
+    public class IndexModel : PageModel
+    {
+        // TODO: injetar o service/repository via construtor
+        // private readonly ICidadaoService _service;
+        // public IndexModel(ICidadaoService service) => _service = service;
+ 
+        // TODO: substituir por lista real vinda do banco
+        // public IEnumerable<Cidadao> Cidadaos { get; set; }
+ 
+        public void OnGet()
+        {
+            // TODO: carregar dados
+            // Cidadaos = _service.ListarTodos();
+        }
+    }
+}
