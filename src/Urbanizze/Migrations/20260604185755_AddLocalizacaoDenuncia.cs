1600af3f84f7fbdcdc5d330083fad2483c9f6699
AndreLucas2103 <p4n1c0021@gmail.com>
2026-06-07 22:41:37 -0300

    feat: denuncia

---
+++ src/Urbanizze/Migrations/20260604185755_AddLocalizacaoDenuncia.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Urbanizze.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLocalizacaoDenuncia : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "localizacao",
+                table: "DENUNCIA",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "localizacao",
+                table: "DENUNCIA");
+        }
+    }
+}
